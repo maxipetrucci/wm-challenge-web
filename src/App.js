25141d3747719ba7981546e0c9a1bbfda6d537c0
Maximiliano Petrucci <maxipetrucci>
2021-07-08 12:34:14 -0300

    Creada la logica para traer y mostrar tweets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import Tweet from './components/Tweet';
+import { getLatestTweets } from './network/NetworkAPI';
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+  const tweetsRef = useRef(tweets);
+
+  useEffect(() => {
+    const getOldestTweetId = () => {
+      if (tweetsRef.current.length === 0) {
+        return undefined;
+      }
+  
+      return tweetsRef.current[tweetsRef.current.length - 1].id;
+    }
+
+    getLatestTweets({ fromid: getOldestTweetId()})
+    .then(response => setTweets(response))
+    .catch(e => console.log(e));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Tweet />
+        {tweets.map(t => <Tweet key={t.id} tweet={t} />)}
       </header>
     </div>
   );
