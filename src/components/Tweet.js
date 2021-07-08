8fe1a390e1c1944cac8bd9d357e85447c07e3f11
Maximiliano Petrucci <maxipetrucci>
2021-07-08 12:33:52 -0300

    Agregados los parametros del Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,49 +4,43 @@ import Like from './svgs/Like';
 import Share from './svgs/Share';
 import IconButton from './IconButton';
 
-export default function Tweet() {
+export default function Tweet({ tweet }) {
     return (
         <div className="Tweet">
             <div className="Row">
                 <div className="Column Tweet-user-picture-container">
-                    <img width={46} height={46} className="Tweet-user-picture" alt="Carlitos" src="https://pbs.twimg.com/profile_images/1400796702974619651/1PMm0ru2_normal.jpg" />
+                    <img width={46} height={46} className="Tweet-user-picture" alt={tweet.user.name} src={tweet.user.image_url} />
                 </div>
                 <div className="Column Tweet-container">
-                    <div className="Row">
-                        <span className="Tweet-user">Muhammad Haseeb</span>
-                        <span className="Tweet-username">@MHaseeb1704</span>
-                        <span className="Tweet-separator">·</span>
-                        <span className="Tweet-timestamp">7s</span>
+                        <div className="Row Tweet-header">
+                            <span className="Tweet-user">
+                                <a href={`https://twitter.com/${tweet.user.username}`} className="Tweet-user-link" target="_blank" rel="noreferrer">
+                                    {tweet.user.name}
+                                </a>
+                            </span>
+                            <span className="Tweet-username">
+                                <a href={`https://twitter.com/${tweet.user.username}`} className="Tweet-username-link" target="_blank" rel="noreferrer">
+                                    @{tweet.user.username}
+                                </a>
+                            </span>
+                            <span className="Tweet-separator">·</span>
+                            <span className="Tweet-timestamp">
+                                <a href={`https://twitter.com/${tweet.user.username}/status/${tweet.id}`} className="Tweet-timestamp-link" target="_blank" rel="noreferrer">{tweet.created_at}</a>
+                            </span>
+                        </div>
                     
-                    </div>
                     <div className="Row Tweet-message">
-                        I knew some basic Python before 2018. Variables, data types… got confused around for loops and functions. Went into JavaScript hard last year, though I dabbled in 2019. Until now, knew even some intermediate JavaScript things (MERN bootcamp grad). I approach in a few ways.
+                        {tweet.text}
                     </div>
                     <div className="Row Tweet-public-metrics">
-                        <IconButton icon={Reply} activeColor='#1da1f2' count={0} />
-                        <IconButton icon={Retweet} activeColor='#17bf63' count={25} />
-                        <IconButton icon={Like} activeColor='#e0245e' />
-                        <IconButton icon={Share} activeColor='#1da1f2' />
+                        <IconButton icon={Reply} activeColor='#1da1f2' count={tweet.replies_count} />
+                        <IconButton icon={Retweet} activeColor='#17bf63' count={tweet.retweets_count} />
+                        <IconButton icon={Like} activeColor='#e0245e' count={tweet.likes_count} />
+                        <IconButton icon={Share} activeColor='#1da1f2' count={tweet.quotes_count} />
                     </div>
                 </div>
             </div>
             
         </div>
     )
-
-    /*
-        <div className="Tweet-container">
-            <span className="Tweet-user">Muhammad Haseeb</span>
-            <span className="Tweet-username">@MHaseeb1704</span>
-            <span className="Tweet-separator">·</span>
-            <span className="Tweet-timestamp">7s</span>
-            <div className="Tweet-message">Python and Javascript</div>
-            <div className="Tweet-public-metrics">
-                <IconButton icon={Reply} activeColor='#1da1f2' />
-                <IconButton icon={Retweet} activeColor='#17bf63' />
-                <IconButton icon={Like} activeColor='#e0245e' />
-                <IconButton icon={Share} activeColor='#1da1f2' />
-            </div>
-        </div>
-    */
 }
