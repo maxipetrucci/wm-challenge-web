b65f6ce5a2d56fe3b4320d803773a5b96229ab8a
Maximiliano Petrucci <maxipetrucci>
2021-07-09 17:46:13 -0300

    Cambiado el nombre del parametro maxresults a limit

--- src/network/NetworkAPI.js
+++ src/network/NetworkAPI.js
@@ -2,5 +2,5 @@ import { fetchFromURL } from './FetchAPI';
 import AppConstants from '../AppConstants';
 
 export const getLatestTweets = async({ fromid, limit }) => {
-    return fetchFromURL(`${AppConstants.API_BASE_URL}/tweets/latest`, { fromid: fromid, maxresults: limit });
+    return fetchFromURL(`${AppConstants.API_BASE_URL}/tweets/latest`, { fromid, limit });
 }
