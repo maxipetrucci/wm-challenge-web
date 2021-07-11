83e93337f27d648b2246220c73eaac01a6b088cc
Maximiliano Petrucci <maxipetrucci>
2021-07-11 18:24:27 -0300

    Corregido el @ que sobraba

--- src/components/UserLink.js
+++ src/components/UserLink.js
@@ -4,7 +4,7 @@ export default function UserLink({ label, username, verified, className }) {
     return (
         <>
             <a href={`https://twitter.com/${username}`} target="_blank" rel="noreferrer" className={className}>
-                @{label || username}
+                {label || `@${username}`}
             </a>
             {verified &&
                 <span className="Tweet-user-verified">
